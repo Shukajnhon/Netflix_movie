a20492764e789eff2a1221fe0fe6c02b57dad939
Shukajnhon <hieupham29697@gmail.com>
2022-04-17 22:35:13 +0700

    responsive header logo and navbar

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const slider = $('.slider');
+const btnLeft = $('#move-left');
+const btnRight = $('#move-right');
+const indicators = $$('.indicator')
